cf086e6b8427a9a7a25e2a78bab8216fb0179c3d
Thom Rawson <t.rawson@imperial.ac.uk>
2024-10-01 21:11:11 +0100

    Minor prior update

--- src/LHC-incidence/LHC-incidence.R
+++ src/LHC-incidence/LHC-incidence.R
@@ -45,10 +45,10 @@ Parameters_df$density <- rep(NA, LHS_samples)
 ## (also known as the quantile function) of the Beta distribution.
 ## Using the qbeta() function, which returns the quantile corresponding to a given
 ## probability (in this case, the uniform values)
-Parameters_df$alpha <- qbeta(Parameters_df$alpha , shape1 = 1, shape2 = 25)
-Parameters_df$beta <- 0.5 + (3 - 0.5) * Parameters_df$beta
+Parameters_df$alpha <- qbeta(Parameters_df$alpha , shape1 = 1, shape2 = 20)
+Parameters_df$beta <- 0.5 + (3.5 - 0.5) * Parameters_df$beta
 Parameters_df$gamma <- 0 + (2.5 - 0) * Parameters_df$gamma
-Parameters_df$sigma <- 0 + (4 - 0) * Parameters_df$sigma
+Parameters_df$sigma <- 0 + (5 - 0) * Parameters_df$sigma
 Parameters_df$asc_rate <- qbeta(Parameters_df$asc_rate , shape1 = 5, shape2 = 1)
 Parameters_df$dispersion <- qexp(Parameters_df$dispersion, rate = 1/0.5)
 
